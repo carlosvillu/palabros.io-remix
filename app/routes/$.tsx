528bff12655bdc6d08338d779fc1ce4c7e5fcafe
Carlos Villuendas Zambrana <carlosvillu@gmail.com>
2023-01-05 11:41:36 +0100

    add metas

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,12 +1,23 @@
 import {search} from '~/palabros/js/http'
-import {fromPathToFilter} from '~/palabros/js/strings'
+import {fromPathToFilter, fromFilterToTitle} from '~/palabros/js/strings'
 import type {DataFunctionArgs} from "@remix-run/node";
 import App, {links as appLinks} from '../palabros/App'
+import type {MetaFunction} from "@remix-run/node";
 
 export const links = () => [
   ...appLinks(),
   {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css'}
 ]
+
+export const meta: MetaFunction = ({data, location}) => {
+  const title = fromFilterToTitle(fromPathToFilter(location.pathname))
+
+  return {
+    description: data.results?.length === 0 ? `▶️  ${title} ◀️ `
+      : `🔍 con (${data.results.length}) resultados para ${title}. 🔥 ¡Seguro que vas a poder acabar ese crucigramas!`
+  }
+
+}
 
 export const loader = async ({context, params, request}: DataFunctionArgs) => {
   const filter = fromPathToFilter(new URL(request.url).pathname)
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,19 @@
+import {fromPathToFilter, fromFilterToTitle} from '~/palabros/js/strings'
+import type {MetaFunction} from "@remix-run/node";
 import App, {links as appLinks} from '../palabros/App'
 
 export const links = () => [
   ...appLinks(),
   {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css'}
 ]
+
+export const meta: MetaFunction = () => {
+  const title = fromFilterToTitle(fromPathToFilter(location.pathname))
+
+  return {
+    description: `▶️  ${title} ◀️ `
+  }
+}
 
 export default function Index() {
   return <App />
